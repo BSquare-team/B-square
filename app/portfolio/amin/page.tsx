98da97bc1f9e1a821b2a83c8803238bf90981435
amin bagheri <amin.b9383@gmail.com>
2026-05-20 14:43:47 +0330

    Add comments section

--- app/portfolio/amin/page.tsx
+++ app/portfolio/amin/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import pic1 from "@/assets/image/avatar-1.webp";
 import pic2 from "@/assets/image/avatar-2.webp";
 import pic3 from "@/assets/image/avatar-3.webp";
@@ -9,10 +11,79 @@ import pic5 from "@/assets/image/avatar-5.webp";
 import picMe from "@/assets/image/Clipped_image_20260519_193224.png";
 import picMe2 from "@/assets/image/mePic.jpg";
 import picMe3 from "@/assets/image/mePic3.png";
+import hp6 from "@/assets/image/hp6.jpg";
+import hp5 from "@/assets/image/hp5.jpg";
+import hp4 from "@/assets/image/hp4.jpg";
+import hp3 from "@/assets/image/hp3.jpg";
+import hp2 from "@/assets/image/hp2.jpg";
+
+import cp1 from "@/assets/image/cp1.jpg";
+import cp2 from "@/assets/image/cp2.jpg";
+import cp3 from "@/assets/image/cp3.jpg";
+import cp4 from "@/assets/image/hp3.jpg";
+import cp5 from "@/assets/image/cp5.jpg";
+import cp6 from "@/assets/image/cp6.jpg";
+import cp7 from "@/assets/image/cp7.jpg";
+import cp8 from "@/assets/image/cp8.jpg";
+import cp9 from "@/assets/image/cp9.jpg";
 
 import { LucideStar, Star } from "lucide-react";
 
 export default function page() {
+  const [comments, setComments] = useState([
+    {
+      profile: cp5,
+      name: "Hamed Tavakoli",
+      job: "NYTimes Best sellers’ video editor",
+      comment: "That was a perfect edit, well done amin",
+      link: " ",
+    },
+    {
+      profile: cp1,
+      name: "Farzane Fazelian",
+      job: "Let’s Grow through My Edits /Video editor helping brands & creators tell their story",
+      comment: "Powerful edit👌",
+      link: " ",
+    },
+
+    {
+      profile: cp2,
+      name: "Mostafa Miri",
+      job: "Podcast/Talking Head Short Form Video Editor",
+      comment: "very clean edit . well done Amin 👏 🔥",
+      link: " ",
+    },
+
+    {
+      profile: cp3,
+      name: "Ali Toorani",
+      job: "Video Editor — I edit videos for cool people!🚀Bringing videos to life through video editing🚀",
+      comment: "Anxiously waiting for your new Videos!",
+      link: " ",
+    },
+
+    {
+      profile: cp4,
+      name: "Mikaeel Jafari",
+      job: "Mechanical Design Engineer | SolidWorks | CAD | Sheet Metal | DFM",
+      comment: "u nailed it!!",
+      link: " ",
+    },
+
+
+    // {
+    //   profile: cp1,
+    //   name: "",
+    //   job: "",
+    //   comment: "",
+    //   link: " ",
+    // },
+  ]);
+
+  const [ShowAll , setShowAll] = useState(false)
+
+ const visibleComments = ShowAll ? comments : comments.slice(0, 2);
+
   return (
     <div className="">
       {/* hero section  */}
@@ -52,7 +123,7 @@ export default function page() {
               {/* ردیف آواتارها و امتیاز */}
               <div className="flex flex-col sm:flex-row items-center gap-6 justify-center lg:justify-start">
                 <div className="flex items-center -space-x-2">
-                  {[pic1, pic2, pic3, pic4, pic5].map((pic, i) => (
+                  {[hp6, hp4, hp3, hp2, hp5].map((pic, i) => (
                     <div
                       key={i}
                       className="relative w-10 h-10 rounded-full border-2 border-white dark:border-gray-900 shadow-md overflow-hidden"
@@ -118,13 +189,108 @@ export default function page() {
             </div>
           </div>
         </div>
-
-        
       </section>
 
       {/* comments  */}
 
-      <div className="h-screen"></div>
+      <section className="relative container mx-auto max-w-7xl mt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-9 px-6">
+          <ul
+            className="space-y-6 col-span-1
+            flex flex-col h-full"
+          >
+            {visibleComments.map((item) => (
+              <>
+                <li className="flex flex-col p-6 gap-4 w-full bg-[hsl(210,40%,95%)] dark:bg-[#161f2c] rounded-2xl ">
+                  <div className="flex flex-row justify-between gap-3 ">
+                    <div className="rounded-full w-10 h-10  overflow-hidden">
+                      <Image
+                        src={item.profile}
+                        alt={item.name}
+                        width={40}
+                        height={40}
+                        className="object-cover h-full"
+                      ></Image>
+                    </div>
+                    <div className="flex flex-col flex-1">
+                      <div className="text-base font-semibold">{item.name}</div>
+                      <span className="text-sm/6 text-gray-600 line-clamp-1">
+                        {item.job}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-[15px] ">{item.comment}</div>
+                </li>
+              </>
+            ))}
+          </ul>
+          <ul
+            className="space-y-6 col-span-1
+            flex flex-col h-full"
+          >
+            {visibleComments.map((item) => (
+              <>
+                <li className="flex flex-col p-6 gap-4 w-full bg-[hsl(210,40%,95%)] dark:bg-[#161f2c] rounded-2xl ">
+                  <div className="flex flex-row justify-between gap-3 ">
+                    <div className="rounded-full w-10 h-10  overflow-hidden">
+                      <Image
+                        src={item.profile}
+                        alt={item.name}
+                        width={40}
+                        height={40}
+                        className="object-cover h-full"
+                      ></Image>
+                    </div>
+                    <div className="flex flex-col flex-1">
+                      <div className="text-base font-semibold">{item.name}</div>
+                      <span className="text-sm/6 text-gray-600 line-clamp-1">
+                        {item.job}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-[15px] ">{item.comment}</div>
+                </li>
+              </>
+            ))}
+          </ul>
+          <ul
+            className="space-y-6 col-span-1
+            flex flex-col h-full"
+          >
+            {visibleComments.map((item) => (
+              <>
+                <li className="flex flex-col p-6 gap-4 w-full bg-[hsl(210,40%,95%)] dark:bg-[#161f2c] rounded-2xl ">
+                  <div className="flex flex-row justify-between gap-3 ">
+                    <div className="rounded-full w-10 h-10  overflow-hidden">
+                      <Image
+                        src={item.profile}
+                        alt={item.name}
+                        width={40}
+                        height={40}
+                        className="object-cover h-full"
+                      ></Image>
+                    </div>
+                    <div className="flex flex-col flex-1">
+                      <div className="text-base font-semibold">{item.name}</div>
+                      <span className="text-sm/6 text-gray-600 line-clamp-1">
+                        {item.job}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-[15px] ">{item.comment}</div>
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+        <div className="inset-x-0 bottom-0 flex justify-center bg-linear-to-t from-white pt-32 pb-8 pointer-events-none dark:from-[#030712] absolute">
+          <Button onClick={()=>setShowAll(!ShowAll)} className="relative pointer-events-auto" > {ShowAll ? "show less" : "Show more ..."} </Button>
+        </div>
+      </section>
+
+      <section className="container m-auto max-w-7xl mt-9">
+        <div className="h-screen"></div>
+      </section>
     </div>
   );
 }
