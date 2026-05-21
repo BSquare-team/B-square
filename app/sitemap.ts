e6e7ea39be1e14e7ca182134321430a279ba2219
Omid Bagheri <omid.b193@gmail.com>
2026-05-22 03:23:15 +0430

    chang sitemap & add vercel.json

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,33 @@
+// app/sitemap.ts
+
 import { MetadataRoute } from "next";
 import { SERVER_URL } from "@/lib/constants";
+import { getAllPosts } from "@/lib/posts";
 
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/admin/",
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = getAllPosts();
+
+  const staticRoutes: MetadataRoute.Sitemap = [
+    {
+      url: SERVER_URL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
-    sitemap: `${SERVER_URL}/sitemap.xml`,
-  };
+    {
+      url: `${SERVER_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+  ];
+
+  const postRoutes: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${SERVER_URL}/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: "weekly" as const,
+    priority: 0.7,
+  }));
+
+  return [...staticRoutes, ...postRoutes];
 }
