1824e5a631d49548eb5d5496231615543c3634a0
amin bagheri <amin.b9383@gmail.com>
2026-05-22 22:27:01 +0330

    Changelog page first try

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+const config = {
+  plugins: [
+    require('@nauverse/tailwind-dot-grid-backgrounds')
+    // سایر پلاگین‌های شما می‌توانند در اینجا باشند
+  ],
+};
+
+module.exports = config;
