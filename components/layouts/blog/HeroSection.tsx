a5948a3db911381b2fafa62e7dbc5fc46a519173
Omid Bagheri <omid.b193@gmail.com>
2026-05-23 04:30:43 +0430

    clean up part 1

---
+++ components/layouts/blog/HeroSection.tsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+  return (
+    <section className="p-6 ">
+      <div className="flex flex-col justify-center prose prose-h1:mb-2 dark:prose-invert">
+        <p>Blog</p>
+        <h1>Whats happening</h1>
+        <p>
+          Stay informed with product updates, company news, and insights on how
+          to sell smarter at your company.
+        </p>
+      </div>
+    </section>
+  );
+}
