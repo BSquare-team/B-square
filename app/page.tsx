8f9c758e2e8257b0719257ea698d0764cf43e203
amin bagheri <amin.b9383@gmail.com>
2026-05-25 00:05:58 +0330

    home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "../styles/home.module.css";
+export default function HomePage() {
+  return (
+    <div className={styles["home-gradient"]}>
+      home page
+    </div>
+  )
+}
