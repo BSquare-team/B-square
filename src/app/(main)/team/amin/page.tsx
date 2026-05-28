a183a3ecb3ded81f8197987d406e59e31bcce8ed
amin bagheri <amin.b9383@gmail.com>
2026-05-29 01:35:40 +0330

    change urls

--- src/app/(main)/team/amin/page.tsx
+++ src/app/(main)/team/amin/page.tsx
@@ -144,25 +144,20 @@ export default function page() {
           </div>
 
           <Link
-            href="/blog/my-journey-through-iran-s-education-system-and-medical-field-a-raw-personal-story"
-            className="md:w-[40%] group"
+            href="/blog/how-i-got-here"
+            className="md:w-[40%] group mt-4"
           >
             <div className="rounded-3xl overflow-hidden hover:shadow-2xl shadow-lg dark:bg-[#0a0a0a94] dark:shadow-[0_10px_30px_-10px_rgba(255,255,255,0.03)] dark:hover:shadow-[0_20px_50px_-12px_rgba(255,255,255,0.08)] transition-all duration-300 h-full">
               <div className="p-8 border-t-4 border-blue-800 flex flex-col justify-between h-full">
                 <div>
                   <p className="text-sm/5 text-gray-500 dark:text-gray-400">
-                    My Journey
+                   
                   </p>
                   <h2 className="mt-2 text-xl/7 font-bold text-gray-900 dark:text-white line-clamp-3 group-hover:text-blue-400 transition-colors">
-                    My Journey Through Iran's Education System and Medical
-                    Field: A Raw Personal Story
+                    How I got here
                   </h2>
                   <p className="mt-2 text-sm/6 text-gray-600 dark:text-gray-400 line-clamp-4">
-                    Almost everyone around me believed that education was the
-                    only safe path toward becoming someone valuable. And
-                    honestly, I believed it too. I wanted to become skilled at
-                    something meaningful. I wanted a future I could be proud of.
-                    But the education system I grew up in was deeply broken.
+                    
                   </p>
                 </div>
                 <p className="mt-6 text-sm/5 font-medium text-gray-700 dark:text-gray-300">
@@ -183,23 +178,18 @@ export default function page() {
             </div>
           </div>
 
-          <Link href="/blog" className="md:w-[40%] group">
+          <Link href="/blog/first-real-client" className="md:w-[40%] group mt-4">
             <div className="rounded-3xl overflow-hidden hover:shadow-2xl shadow-lg dark:bg-[#0a0a0a94] dark:shadow-[0_10px_30px_-10px_rgba(255,255,255,0.03)] dark:hover:shadow-[0_20px_50px_-12px_rgba(255,255,255,0.08)] transition-all duration-300">
               <div className="p-8 border-t-4 border-blue-800 flex flex-col justify-between">
                 <div>
                   <p className="text-sm/5 text-gray-500 dark:text-gray-400">
-                    My Journey
+                
                   </p>
                   <h2 className="mt-2 text-xl/7 font-bold text-gray-900 dark:text-white line-clamp-3 group-hover:text-blue-400 transition-colors">
-                    My Journey Through Iran's Education System and Medical
-                    Field: A Raw Personal Story
+                  My first real client
                   </h2>
                   <p className="mt-2 text-sm/6 text-gray-600 dark:text-gray-400 line-clamp-4">
-                    Almost everyone around me believed that education was the
-                    only safe path toward becoming someone valuable. And
-                    honestly, I believed it too. I wanted to become skilled at
-                    something meaningful. I wanted a future I could be proud of.
-                    But the education system I grew up in was deeply broken.
+   
                   </p>
                 </div>
                 <p className="mt-6 text-sm/5 font-medium text-gray-700 dark:text-gray-300">
