efb7c29f130b31ab233b5b96962317d5623b761f
amin bagheri <amin.b9383@gmail.com>
2026-06-06 01:22:11 +0330

    home page change texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,13 @@ export default function HomePage() {
     <div className="home-gradient">
       <section className="relative mx-auto lg:w-[1020px] px-6 flex items-center h-screen justify-center md:justify-start ">
         <div className="flex flex-col w-full lg:w-[41%] sm::w-[64%] text-center md:text-start items-center md:items-start prose dark:prose-invert">
-          <h1>Powerful tools for web professionals</h1>
+          <h1>ELEVATING BRANDS. Grow like a Fortune 500.</h1>
           <p>
-            We help designers and developers finish their projects on time and
-            ship faster.
+            We help designers and developers ship faster, solve problems
+            cleanly, and build brands that compete with the best. 
           </p>
           <Link href={"/team"}>
-            <Button
-              className="w-44.25 rounded-full bg-[#2d53fe] hover:bg-[#0533ff] border-0 shadow-2xl shadow-[#2d53fe]"
-            >
+            <Button className="w-44.25 rounded-full bg-[#2d53fe] hover:bg-[#0533ff] border-0 shadow-2xl shadow-[#2d53fe]">
               View our profiles
             </Button>
           </Link>
