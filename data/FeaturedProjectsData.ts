5f76474d43b34549231585df542bc1d7853132a4
amin bagheri <amin.b9383@gmail.com>
2026-06-17 23:54:45 +0330

    fixing Embed problem ##educational

--- data/FeaturedProjectsData.ts
+++ data/FeaturedProjectsData.ts
@@ -4,7 +4,7 @@ export const ProjectsAminData = [
     bottomText:
       "I re-edited one of Adam Bensman’s talks on pitching. The goal: make the message sharper, more engaging, and ready for short-form platforms. This can work as a hook, a teaser, or a standalone mini-version.",
     YoutubeEmbed:
-      "https://www.youtube.com/embed/uQBPZqRhAr4?si=-R-agwfGJLg624s6",
+      "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7395027531776585728?compact=1",
     thumbNail: "D14-1.webm",
   },
   {
@@ -12,7 +12,7 @@ export const ProjectsAminData = [
     bottomText:
       "A crisp 40s explainer that turns a complex compliance product into something prospects actually want to watch  and act on",
     YoutubeEmbed:
-      "https://www.youtube.com/embed/uQBPZqRhAr4?si=-R-agwfGJLg624s6",
+      "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7385569211575697408?compact=1",
     thumbNail: "ui-style.webm",
   },
   {
@@ -20,7 +20,7 @@ export const ProjectsAminData = [
     bottomText:
       "One year of advertising for Ryze Mushroom Coffee. Over 100 short videos — each with motion graphics, voiceover, and fast cuts.",
     YoutubeEmbed:
-      "https://www.youtube.com/embed/uQBPZqRhAr4?si=-R-agwfGJLg624s6",
+      "",
     thumbNail: "Ryzepreview-1.webm",
   },
   {
@@ -28,7 +28,7 @@ export const ProjectsAminData = [
     bottomText:
       "A deep dive into Facebook's data harvesting, Cambridge Analytica, and Mark Zuckerberg's journey. Edited for pacing, retention, and clarity — turning a 12-minute story into a tight, watchable narrative.",
     YoutubeEmbed:
-      "https://www.youtube.com/embed/uQBPZqRhAr4?si=-R-agwfGJLg624s6",
+      "",
     thumbNail: "Zuckerberg.webm",
   },
 ];
