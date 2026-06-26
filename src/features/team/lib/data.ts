523a109b81e2c941b3a2a7fa682ef782278b4853
amin bagheri <amin.b9383@gmail.com>
2026-06-26 22:04:59 +0330

    amin on top

--- src/features/team/lib/data.ts
+++ src/features/team/lib/data.ts
@@ -2,16 +2,17 @@ import omidPic from "@/src/assets/image/omidPic.webp";
 import aminPic from "@/src/assets/image/mePic.jpg";
 
 export const members = [
+   {
+    name: "Amin",
+    role: "Creative Developer",
+    image: aminPic,
+    href: "/team/amin",
+  },
   {
     name: "Omid",
     role: "Frontend Engineer",
     image: omidPic,
     href: "/team/omid",
   },
-  {
-    name: "Amin",
-    role: "Creative Developer",
-    image: aminPic,
-    href: "/team/amin",
-  },
+ 
 ];
